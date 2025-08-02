864aa8ef5b5063f57385a1987c85cbcae56e0a13
fouadlizoullizoul <fouadlizoul22@gmail.com>
2025-08-02 07:35:05 +0200

    Conditions and  Statements

---
+++ Conditions/Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+    public static void main(String[] args){
+       int Age =25;
+       int votingAge =18;
+
+       if(Age >=votingAge){
+        System.out.println("Old enough to votel");
+       }else{
+        System.out.println("Not old enough to vote.");
+       }
+
+       int n =4 ;
+       int result = 0;
+        result = n%2==0 ?  10 :  20;
+        System.out.println(result);
+
+    }
+}
--- Operators/Operators.java
+++ Operators/Operators.java
@@ -1,6 +1,8 @@
 public class Operators {
+    
     public static void main(String[] args) {
         int x =4 ;
-        System.out.println(x>3 && x <5);
+        int y =5;
+        System.out.println(x>3 || y <=5);
     }
 }
