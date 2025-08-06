cb1e60dfe13a3f8711005af9863776fdf00db655
fouadlizoullizoul <fouadlizoul22@gmail.com>
2025-08-06 14:34:14 +0200

    Arrays calculates the average of different ages

--- Hello.java
+++ Hello.java
@@ -6,7 +6,22 @@ class inf{
         int age = 21;
         float salry = 900.1f;
         System.out.println("Your name is " + name + " " + lname + " and your age is " + age + " and your salry is " + salry);
+        int nums[][] =new int[3][];
+        nums[0]=new int[3];
+        nums[1]=new int[4];
+        nums[2]=new int[2];
 
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums[i].length;j++){
+                nums[i][j]=(int)(Math.random() *10);
+            }
+        }
+        for(int n[] : nums){
+            for(int m:n){
+                System.out.println(m + " ");
+            }
+            System.out.println();
+        }
     }
 } 
     
