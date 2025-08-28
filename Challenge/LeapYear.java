a1d5f6c50515fc1866e58c4d67e2f0f6fc1f5bcd
fouadlizoullizoul <fouadlizoul22@gmail.com>
2025-08-28 13:48:29 +0200

    Leap Year  Challenge

---
+++ Challenge/LeapYear.java
@@ -0,0 +1,11 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(2023));
+    }
+    public static boolean isLeapYear (int year){
+             return (year >=1 && year <=9999) && (year % 4 == 0 && (year % 100 != 0 || year % 400 ==0));
+
+        
+    }
+    
+}
