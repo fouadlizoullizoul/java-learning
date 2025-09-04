6739106b7bed6515cccaf67f18163d4111af37f8
fouadlizoullizoul <fouadlizoul22@gmail.com>
2025-09-04 13:21:49 +0200

    Area Calculator For Circles And Rectangles

---
+++ Challenge/AreaCalculator.java
@@ -0,0 +1,18 @@
+public class AreaCalculator {
+    public  static void main(String[] args){
+        System.out.println(area(8.5));
+        System.out.println(area(5.0 ,4.0));
+    }
+    public static  double area(double radius){
+        if (radius <0){
+            return  -1;
+        }
+        return (radius  * radius ) *3.14159;
+    }
+    public  static  double area(double x , double y){
+        if(x <0 || y <0){
+            return  -1;
+        }
+        return  x * y;
+    }
+}
